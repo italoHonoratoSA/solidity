fbbe0d89fcd3e6fe507699250ed2cf03579e79d2
Ítalo Honorato <89170101+italoHonoratoSA@users.noreply.github.com>
2022-02-22 19:01:02 -0300

    Solidity standard to implement airdrop functions

--- aidrop-code-solidity.sol
+++ aidrop-code-solidity.sol
@@ -12,12 +12,12 @@ contract ExampleLinesForAirdrop {
 
 // @dev Airdrop function that makes the best verification of the positions [i] for locating and distributing the tokens
 // @param calldata is necessary to indicate the explicit location of data for the variables of structure types 'address' and 'uint256'.
-// @param An indication of the explicit localization of variables around it is mandatory as of solidity v0.5. See more at https://docs.soliditylang.org/en/v0.5.0/050-breaking-changes.html
+// @param An indication of the explicit localization of variables is mandatory as of solidity v0.5. See more at https://docs.soliditylang.org/en/v0.5.0/050-breaking-changes.html
 interface IERC20 {
 
 }
 
-// No scheme to use the ERC20 and IERC20 standard of OpenZeppenlin
+// Don't forget to use OpenZeppenlin's ERC20 and IERC20 standard
 contract ERC20 is IERC20 {
     using SafeMath for uint256;
 
@@ -43,8 +43,8 @@ function drop1 (address from, address[] calldata addresses, uint256[] calldata t
 
     }
 
-// @dev This function is different from the previous one due to the economy of the process in the EVM, reducing the cost of gas payments,
-// mainly in the first case of the function for the parameters.
+// @dev This function differs from the previous one by the economy of processing in the EVM, reducing the costs paid for gas,
+// mainly on the first call of the function to the parameters.
 function drop2 (address from, address[] calldata addresses, uint256[] calldata tokens) external onlyOwner {
 
         uint256 SCCC = 0;
